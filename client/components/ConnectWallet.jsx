624d47402578dd8a4ea5a879fa81320e8311ba17
SeniorDevCode <senior.dev.software@gmail.com>
2021-05-24 17:53:39 +0530

    add: copyright message

--- client/components/ConnectWallet.jsx
+++ client/components/ConnectWallet.jsx
@@ -76,7 +76,7 @@ const ConnectWallet = ({
     try {
       const externalProvider = await web3Modal.connect();
 
-      const biconomy = new Biconomy(new Web3.providers.HttpProvider("https://rpc-mainnet.maticvigil.com")
+      const biconomy = new Biconomy(new Web3.providers.HttpProvider("https://rpc-mainnet.matic.network")
         , { apiKey: process.env.biconomy_api_key, debug: true });
       setBiconomyProvider(new Web3(biconomy));
 
